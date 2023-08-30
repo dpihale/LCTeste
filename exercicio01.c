130ba80aacc8ec3f8e0bee8acf489b7ec270f1b9
dpihale <djocodjolourenco@gmail.com>
2023-08-30 16:16:43 +0100

    Exercicios de Diagnostico

---
+++ exercicio01.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+    int matrix[4][8], soma[4], aux=0, i,j;
+
+    for(i=0;i<4;i++)
+    {
+       
+        for(j=0;j<8;j++)
+        {   printf("linha: %d coluna: %d\n",i,j);
+            printf("dgite qualquer numero: ");
+            scanf("%d",&matrix[i][j]);
+            aux = aux+  matrix[i][j];
+        }
+        soma[i]= aux;
+        aux = 0;
+    
+    }
+    printf("mostrar os elementos das matrizes: \n");
+    for(i=0;i<4;i++)
+    {
+        for(j=0;j<8;j++)
+        {
+            printf("%d ",matrix[i][j]);
+        }
+        printf("\n");
+    }
+    printf("soma das linhas: \n");
+
+    for(i=0;i<4;i++){
+        printf("%d\n",soma[i]);
+    }   
+
+}
---
+++ exercicio2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+int main()
+{
+    float matrix[5][4];
+    int count=1;
+    float numero;
+    for(int i=0;i<5;i++)
+    {
+        printf("informe as 4 notas do %d o aluno: \n",count++);
+        for(int j=0;j<4;j++)
+        {
+            printf("informe um numero: ");
+            scanf("%f",&numero);
+            if(numero>=0 && numero<=20)
+            {
+                matrix[i][j] = numero;
+            }
+            else
+            {
+                printf("Nota invalida!!!\n");
+                if(j>=1)
+                {
+                    j--;
+                }
+            }
+
+        }
+    }
+
+    count=1;
+
+    for(int i=0;i<5;i++)
+    {
+        printf("notas do %do aluno:\n", count++);
+        for(int j=0;j<4;j++)
+        {
+            printf("nota: %.2f \n",matrix[i][j]);
+        }
+    }
+}
